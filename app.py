3464a253c1287b4ccb0c0360fc2548acd925a57b
s166harth <siddharthsagar2019@gmail.com>
2022-03-16 15:44:20 +0530

    standard dates

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_bollinger_bands(prices, rate=20):
 def main():
     st.title('Ticker')
 
-    start = st.date_input('Start date')
+    start = st.date_input('Start date','2019-01-18')
     end = st.date_input('End date')
     ticker = st.text_input('Enter Ticker','AAPL')
 
